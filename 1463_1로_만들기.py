2932f5d35913ec43ab4078a2eee91861b6d23d5c
gumsu <chavi9@naver.com>
2021-01-02 15:08:24 +0900

    1463. 1로 만들기

---
+++ 1463_1로_만들기.py
@@ -0,0 +1,14 @@
+num = int(input())
+count = 0
+
+num -=1
+count +=1
+while num != 1:
+    if num % 3 == 0:
+        num = num / 3
+        count += 1
+    elif num % 2 == 0:
+        num = num / 2
+        count += 1
+
+print(count)
