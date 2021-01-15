679b4bd2e3598648273837cc44f15b818ab60c19
gumsu <chavi9@naver.com>
2021-01-16 01:27:11 +0900

    [#15] 15663. N과 M (9) - 2

--- 15663_N과_M_(9).py
+++ 15663_N과_M_(9).py
@@ -1,20 +1,20 @@
-def DFS(L, check, res):
+def DFS(L, check, res, maxNum):
     global answer
-    used = [False]*10001
     if L == m:
         answer += res + '\n'
         return
-    for i in range(n):
-        if not check[i] and not used[a[i]]:
-            used[a[i]] = True
-            check[i] = True
-            DFS(L+1, check, res+str(a[i])+' ')
-            check[i] = False
+    for i in range(1, maxNum+1):
+        if check[i] != 0:
+            check[i] -= 1
+            DFS(L+1, check, res+str(i)+' ', maxNum)
+            check[i] += 1
 if __name__=="__main__":
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
-    a.sort()
+    check = [0]*10001
+    maxNum = max(a)
+    for i in a:
+        check[i] += 1
     answer = ""
-    check = [False]*n
-    DFS(0, check, "")
+    DFS(0, check, "", maxNum)
     print(answer)
