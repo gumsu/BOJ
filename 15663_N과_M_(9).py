eea00dff716585d89c87d1ceb48550f2e9951528
gumsu <chavi9@naver.com>
2021-01-21 21:22:18 +0900

    [#16] 15663. N과 M (9) - 3

--- 15663_N과_M_(9).py
+++ 15663_N과_M_(9).py
@@ -1,13 +1,15 @@
-def DFS(L, check, res, maxNum):
+def DFS(L, check, res):
     global answer
     if L == m:
         answer += res + '\n'
         return
     for i in range(1, maxNum+1):
-        if check[i] != 0:
-            check[i] -= 1
-            DFS(L+1, check, res+str(i)+' ', maxNum)
-            check[i] += 1
+        if check[i] == 0:
+            continue
+        check[i] -= 1
+        DFS(L+1, check, res + str(i)+ ' ')
+        check[i] += 1
+
 if __name__=="__main__":
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
@@ -16,5 +18,5 @@ if __name__=="__main__":
     for i in a:
         check[i] += 1
     answer = ""
-    DFS(0, check, "", maxNum)
+    DFS(0, check, "")
     print(answer)
