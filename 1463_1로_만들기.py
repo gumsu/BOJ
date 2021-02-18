f3a27bed0c9576a4edeb9e66c5a2a3dc1a5e20a0
gumsu <chavi9@naver.com>
2021-02-18 23:21:41 +0900

    [#20] 1463. 1로 만들기

--- 1463_1로_만들기.py
+++ 1463_1로_만들기.py
@@ -1,14 +1,19 @@
-num = int(input())
-count = 0
+# f(x-1) + 1
+# f(x/2) + 1
+# f(x/3) + 1
+# 함수들 중 최솟값을 구한다.
+# f(x) = min(f(x/3), f(x/2), f(x-1)) + 1
 
-num -=1
-count +=1
-while num != 1:
-    if num % 3 == 0:
-        num = num / 3
-        count += 1
-    elif num % 2 == 0:
-        num = num / 2
-        count += 1
+n = int(input())
+dp = [0]*(n+1)
 
-print(count)
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1 # 먼저 이전 단계 +1을 다 넣어준다.
+
+    # 3의 배수이거나 2의 배수라면 바꾼다.
+    if i%3 == 0:    # 3의 배수라면
+        dp[i] = min(dp[i], dp[int(i/3)] +1) # 이전 단계 +1 과 3배수 + 1 중 더 작은 수 택한다.
+    if i%2 == 0:
+        dp[i] = min(dp[i], dp[int(i/2)] +1)
+
+print(dp[n])
