702be93bbdf9ec771d6e15b2c7df18ad2e7ce907
gumsu <chavi9@naver.com>
2021-03-04 20:01:58 +0900

    [#26] 10814. 나이순 정렬

---
+++ 10814_나이순_정렬.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = []
+for i in range(n):
+    age, name = input().split()
+    arr.append((int(age), name, i))
+
+arr.sort(key=lambda x: (x[0],x[2]))
+
+for a, b, c in arr:
+    print(a, b)
