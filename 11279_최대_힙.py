2a609b2943aed5c3eca73070119f1ea52c0d02b2
gumsu <chavi9@naver.com>
2021-03-18 22:01:05 +0900

    [#31] 11279. 최대 힙

---
+++ 11279_최대_힙.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+
+n = int(input())
+q = []
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if not q:
+            print(0)
+        else:
+            print(-heapq.heappop(q))
+    else:
+        heapq.heappush(q, -x)
