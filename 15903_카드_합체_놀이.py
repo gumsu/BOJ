4fec6051afb73c1ae8f5177f5c99ae94064d955b
gumsu <chavi9@naver.com>
2022-08-13 20:44:26 +0900

    [#55] 15903. 카드 합체 놀이

---
+++ 15903_카드_합체_놀이.py
@@ -0,0 +1,12 @@
+import heapq
+n, m = map(int, input().split())
+cards = list(map(int,input().split()))
+cards.sort()
+
+for _ in range(m):
+    a = heapq.heappop(cards)
+    b = heapq.heappop(cards)
+    temp = a+b
+    heapq.heappush(cards, temp)
+    heapq.heappush(cards, temp)
+print(sum(cards))
